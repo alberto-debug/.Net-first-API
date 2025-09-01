c750dadea37302f930208b8c7de5b0ffc7700013
alberto-debug <albertojunior848423803@gmail.com>
2025-09-01 09:56:13 +0300

    changes

---
+++ DTOs/AuthorDto.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetAPI.DTOs
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Biography { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string? Nationality { get; set; }
+
+    }
+
+    public class CreateAuthorDto
+    {
+
+        public string Name { get; set; } = string.Empty;
+        public string? Biography { get; set; } 
+        public DateTime DateOfBirth{ get; set; }
+        
+
+    }
+}
